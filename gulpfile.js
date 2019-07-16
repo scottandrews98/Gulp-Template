27de8d9486b20274d85daeb498d976042c372fa6
Scott Andrews <ANDS3_16@UNI.WORC.AC.UK>
2019-07-16 11:56:31 +0100

    nearly got gulp working

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-// Static server
-gulp.task('browser-sync', function() {
+
+// Compiles SASS
+gulp.task('sass', function(){
+    return gulp.src('app/sass/*.scss')
+    .pipe(sass())
+
+    .pipe(gulp.dest('public'))
+
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+})
+
+// Watch For Changes
+gulp.task('watch', gulp.series('sass'), function(){
     browserSync.init({
-        server: {
-            baseDir: "./public"
-        }
-    });
-});
+        server: "public"
+    })
 
+    gulp.watch('app/sass/*.scss', gulp.series('sass'))
+    gulp.watch('public/*.html'.on('change', browserSync.reload))
+    gulp.watch('public/*.js'.on('change', browserSync.reload))
+})
